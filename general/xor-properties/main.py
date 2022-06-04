67e97196f3c50655a9ab8cb7fb4e78a8beecd42f
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2022-06-04 09:12:08 -0400

    this is so hard :(

--- general/xor-properties/main.py
+++ general/xor-properties/main.py
@@ -1,9 +1,14 @@
+import binascii
 
 key1 = 'a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313'
+key1x2 = '37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e'
+key1x2x3 = 'c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1'
+key1x2x3xflag = '04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf'
 
-def hex_to_int(*argv):
-    results = []
-    for arg in argv:
-        results.append(int(bytes.fromhex(arg)))
+def get_bytes(hexst):
+    binascii.unhexlify(hexst)
 
-hex_to_int(key1)
+flag = get_bytes(key1)
+# flag = get_bytes(key1) ^ get_bytes(key1x2) ^ get_bytes(key1x2x3) ^ get_bytes(key1x2x3xflag)
+
+print(flag)
