ca18a34274e3ab1bfcd97fbd3c9e4afe189c488e
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2022-08-16 12:46:15 +0000

    submillisecondf SPEED!

--- rsa/rsa-starter-4/rsa-starter-4.py
+++ rsa/rsa-starter-4/rsa-starter-4.py
@@ -3,11 +3,32 @@ q = 1029224947942998075080348647219
 totient = (p - 1)*(q - 1)
 e = 65537
 
-# find d, when ed = 1 mod totient
 
+# the extended Euclidean Algorithm
+def gcdExtended(a, b):
+    # Base Case
+    if a == 0 :
+        return b,0,1
+             
+    gcd,x1,y1 = gcdExtended(b%a, a)
+     
+    # Update x and y using results of recursive
+    # call
+    x = y1 - (b//a) * x1
+    y = x1
+     
+    #    ax + by = gcd(a, b)
+    return gcd,x,y
+     
+ 
+
+# calculate the gcd of p and q
+gcd, x ,y = gcdExtended(e, totient)
+print(gcd, x, y)
+
+print(x % totient)
+# find d, when ed = 1 mod totient
 # calculate the modular multiplicative inverse of e
-for i in range(totient -1):
-    if(i*e == 1 % totient):
-        print(i)
-        
+
+
 print(e, "d = 1 mod", totient )
